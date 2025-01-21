44cea1011086a3ba1ef0711d23e8dace183f0a4a
thd3r <97145312+thd3r@users.noreply.github.com>
2025-01-21 10:53:34 +0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Rcptchv2Bypss',
+    version=__version__,
+    author='Thunder (@thd3r)',
+    author_email='thd3r@proton.me',
+    description='Rcptchv2Bypss',
+    packages=find_packages(),
+    install_requires=[
+        'rich',
+        'pydub',
+        'asyncio',
+        'selenium',
+        'SpeechRecognition',
+        'webdriver-manager'
+    ],
+    entry_points={
+        'console_scripts': [
+            'rcptchv2bypss = src.main'
+        ]
+    },
+    license='MIT',
+    url='https://github.com/societyprojects/Rcptchv2Bypss',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    keywords=['Rcptchv2Bypss', 'Recaptchav2', 'captchav2']
+)
